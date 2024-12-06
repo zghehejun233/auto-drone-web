cc95eb71bae24a3af39d7ddcb69ea02ee9f73d59
GuoSurui <thuray.email@gmail.com>
2024-12-06 19:58:02 +0800

    feat: 添加无人机管理页面骨架

---
+++ app/[lang]/drones/page.tsx
@@ -0,0 +1,43 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+function StatisticsPanel() {
+  return (
+    <div className="mb-4">
+      <div className="flex gap-4 justify-around">
+        {Array.from({ length: 3 }).map((_, index) => (
+          // <div key={index} className="flex flex-col gap-4">
+          <Skeleton className="h-[192px] w-full rounded-xl" />
+          // </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function DronesList() {
+  return (
+    <div className="my-4">
+      <Skeleton className="h-[900px] w-full rounded-xl" />
+    </div>
+  );
+}
+
+export default function DronesPage() {
+  return (
+    <div className="min-h-screen p-4">
+      <StatisticsPanel />
+      <div className="my-4 flex justify-between">
+        <div className="flex gap-4">
+          {Array.from({ length: 3 }).map((_, index) => (
+            <Skeleton className="h-12 w-[96px] rounded-xl" />
+          ))}
+        </div>
+
+        <div className="flex gap-4">
+          <Skeleton className="h-12 w-[256px] rounded-xl" />
+        </div>
+      </div>
+      <DronesList />
+    </div>
+  );
+}
